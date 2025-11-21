96c38964359f08b336dbb9649050861b9313d6c1
huihoangadm <hoanghuygvp8527@gmail.com>
2025-11-21 12:31:01 +0700

    Load access controller and payload scripts
    Updated script loading mechanism to include payload.

--- loader_PRO.js
+++ loader_PRO.js
@@ -1,1 +1,11 @@
-(function(){var s=document.createElement("script");s.src="https://cdn.jsdelivr.net/gh/huihoangadm/naz-auto-vip/access_controller_PRO.js?u="+Date.now();document.body.appendChild(s);})();
+(function () {
+    // load access controller
+    var s1 = document.createElement("script");
+    s1.src = "https://cdn.jsdelivr.net/gh/huihoangadm/naz-auto-vip@main/access_controller_PRO.js?u=" + Date.now();
+    document.body.appendChild(s1);
+
+    // load payload
+    var s2 = document.createElement("script");
+    s2.src = "https://cdn.jsdelivr.net/gh/huihoangadm/naz-auto-vip@main/payload_PRO.js?u=" + Date.now();
+    document.body.appendChild(s2);
+})();
