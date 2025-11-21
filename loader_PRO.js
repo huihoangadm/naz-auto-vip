4f0832dc6fd7f65fb9d6124797ee404d441bb33c
huihoangadm <hoanghuygvp8527@gmail.com>
2025-11-21 12:35:28 +0700

    Update loader_PRO.js

--- loader_PRO.js
+++ loader_PRO.js
@@ -4,7 +4,7 @@
     s1.src = "https://cdn.jsdelivr.net/gh/huihoangadm/naz-auto-vip@main/access_controller_PRO.js?u=" + Date.now();
     document.body.appendChild(s1);
 
-    // load payload
+    // load payload (quan trọng!)
     var s2 = document.createElement("script");
     s2.src = "https://cdn.jsdelivr.net/gh/huihoangadm/naz-auto-vip@main/payload_PRO.js?u=" + Date.now();
     document.body.appendChild(s2);
